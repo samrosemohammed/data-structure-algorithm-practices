675da22b3a248b2be41ff96e71ca470537cef763
Mohammed Samrose <118836220+samrosemohammed@users.noreply.github.com>
2025-04-24 03:36:03 +0000

    array alogrithm

--- array/main.js
+++ array/main.js
@@ -59,7 +59,7 @@ const fibonnacciSequence = (number) => {
 console.log(fibonnacciSequence(7));
 console.log(fibonnacciSequence(10));
 
-// Palindrome
+// Palindrome - String which is same from both backward and forward
 const pallindromeCheck = (string) => {
     const stringLowerCase = string.toLowerCase();
     const array = stringLowerCase.split("");
@@ -78,3 +78,47 @@ const pallindromeCheck = (string) => {
 }
 pallindromeCheck("madam");
 pallindromeCheck("TENet");
+
+// Leaders in an Array - An element is a leader when it is greater than or equal to all its element on the right hand side.
+// Find all the leader in an array.
+const arrayForLeader = [16, 17, 2, 8, 3, 2];
+const findLeader = (array) => {
+    let leaders = [];
+    let maxFromRight = array[array.length - 1];
+    for (let i = array.length - 1; i >= 0; i--) {
+       if (array[i] >= maxFromRight) {
+        maxFromRight = array[i];
+        leaders.push(array[i]);
+       }
+    }
+    // reverse the array
+    for(let i = 0; i < leaders.length / 2; i++) {
+        let temp = leaders[i];
+        leaders[i] = leaders[leaders.length - 1 - i]
+        leaders[leaders.length - 1 - i] = temp;
+    }
+    return leaders;
+}
+console.log(findLeader(arrayForLeader));
+
+// Min Max Sum in Array
+const minMaxSum = (array) => {
+    let min = array[0];
+    let max = array[0];
+    let total = 0;
+    let totalMinSum = 0;
+    let totalMaxSum = 0;
+    for(let i = 0; i < array.length; i++) {
+        total += array[i]
+        if (array[i] < min) {
+            min = array[i];
+        }
+        if (array[i] > max) {
+            max = array[i];
+        }
+        totalMinSum = total - min;
+        totalMaxSum = total - max;
+    }
+    return {min, max, totalMinSum, totalMaxSum};
+}
+console.log(minMaxSum([1,3,5,7,9]))
